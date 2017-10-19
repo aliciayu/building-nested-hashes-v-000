b182ac1d82a265bf92fe8d4d192eb6da7b886e72
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-19 21:22:31 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -34,7 +34,7 @@ def third_challenge
         #your key/value pairs here
         name: "Juliet",
         age: "15",
-        status: "alive",
+        status: "alive"
         },
       :heroine_friends => []
    }
